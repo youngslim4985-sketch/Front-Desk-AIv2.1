95cde701f7ac6b60b9009b8f08ec48d9f3086ba7
Terrance Franklin <youngslim@gmail.com>
2026-09-10 04:39:47 -0500

    Configure PostgreSQL SSL for production

--- server/db.ts
+++ server/db.ts
@@ -2,6 +2,7 @@ import { Pool } from 'pg';
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
 });
 
 export async function withTenant(companyId, fn) {
